0e51b34ce68f11d6b4f0c96e86ee85b80a89fbbb
Musa <musaharoon.2003@gmail.com>
2024-09-09 22:10:13 +0500

    integrated employees in tenant

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -8,11 +8,11 @@ const adminController = {
   generateCard: async (req, res) => {
     try {
       const sponsor = "NSTP";
-      const { employeeId, validity } = req.body;
-      if (!employeeId || !validity) {
+      const { employeeId, cardNumber, validity } = req.body;
+      if (!employeeId || cardNumber == undefined || !validity) {
         return res
           .status(400)
-          .json({ message: "Please provide employeeID and validity" });
+          .json({ message: "Please provide all fields" });
       }
 
       const employee = await Employee.findById(employeeId);
@@ -24,12 +24,14 @@ const adminController = {
         employee_id: employeeId,
       });
 
+      cardAllocation.card_number = cardNumber;
       cardAllocation.is_issued = true;
       cardAllocation.date_issued = new Date();
 
       await cardAllocation.save();
 
       const card = {
+        cardNumber: cardNumber,
         photo: employee.photo,
         name: employee.name,
         designation: employee.designation,
@@ -52,7 +54,7 @@ const adminController = {
   generateEtag: async (req, res) => {
     try {
       const { employeeId, validity, etagNumber } = req.body;
-      if (!employeeId || !validity || !etagNumber) {
+      if (!employeeId || !validity || etagNumber == undefined) {
         return res.status(400).json({ message: "Please provide all fields" });
       }
 
@@ -65,6 +67,7 @@ const adminController = {
         employee_id: employeeId,
       });
 
+      etagAllocation.etag_number = etagNumber;
       etagAllocation.is_issued = true;
       etagAllocation.date_issued = new Date();
 
